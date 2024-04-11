1eb8e64172afd42353c66a6c499ab869dd641832
Akshay keshav balte <balteakshay1998@gmail.com>
2024-04-11 23:14:59 +0530

    add programs

---
+++ baisc_programs/prime.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    bool prime= true;
+
+    cout<<"Enter the positive  number :"<<endl;
+    cin>>num;
+
+ 	 for(int i=0;i<=num/2;i++)
+    {
+        if(num%i==0)
+            {
+                 prime = false;
+                  break;
+
+            }
+    }
+
+    if(prime)
+    {
+            cout<<num<<" is prime number"<<endl;
+    }
+        
+    else
+    {
+       cout<<num<<" is not  prime number"<<endl;
+    }
+
+
+    return 0;
+}
